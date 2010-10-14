26c5041a1cd18c3ddbad1e07ce6224094c6d9a92
Kaspars <kaspars.dancis@gmail.com>
2010-10-14 13:18:23 +0300

    updated copyright

--- station.py
+++ station.py
@@ -1,4 +1,4 @@
-# Copyright 2009 Kaspars Dancis, Kurt Daal
+# Copyright 2010 Kaspars Dancis
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
