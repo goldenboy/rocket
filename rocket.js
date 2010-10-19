92477d7af5201a967f96b0970e06ec6dbf26e3f4
Kaspars <kaspars.dancis@gmail.com>
2010-10-19 15:36:13 +0300

    reorganized structure

---
+++ rocket.js
@@ -0,0 +1,465 @@
+var Rocket = function() {
+	
+	var	KEY = "key",
+		TIMESTAMP = "timestamp",
+		IS_DIRTY = "is_dirty",
+		RECEIVE = "receive",
+		SEND = "send";
+	
+	
+	
+	var settings = {
+			TABLES: {},
+			DATABASE_NAME: "rocket",
+			LOG: function(m) {
+				// for example: console.log(m);
+			},
+			LOG_ERROR: function(e) {
+				alert(e);
+			},
+			LOG_SQL: false,
+			SYNC_NOW_DELAY_MS: 100
+		}, 
+		db;
+
+	
+	
+	var tableTemplate = {
+		SYNC_IDLE_TIME_MS: 10000, // sync every 10 seconds
+		
+		// number of records to load from AppEngine in a single request, reduce this number if requests are using too much CPU cycles or are timing out
+		RECEIVE_BATCH_SIZE: 150,		
+
+		// number of records to send to AppEngine in a single request, reduce this number if requests are using too much CPU cycles or are timing out
+		SEND_BATCH_SIZE: 150,		
+	};
+		
+	
+
+	function init(_settings) {		
+		merge(_settings, settings);
+
+		if (settings.DATABASE) {
+			db = settings.DATABASE;
+		} else {
+			db = openDatabase(settings.DATABASE_NAME, "", "", 1024*1024);
+		}
+		
+		// init tables
+		var tables = settings.TABLES;
+		for (tableName in tables) {
+			var table = copy(tableTemplate);
+			merge(tables[tableName], table);
+			settings.TABLES[tableName] = table;
+			                  
+			table.TABLE_NAME = tableName;			
+
+			initTable(table);
+		}		
+		
+	}
+	
+	
+	
+	/**
+	 * Helper - merges attributes of one object into another.
+	 */
+	function merge(from, to) {
+		for (attr in from) { 
+			to[attr] = from[attr]; 
+		}
+	}
+	
+	
+	
+	/**
+	 * Helper - copies (clones) an object.
+	 */
+	function copy(obj) {
+		var c = {};
+		
+		for (attr in obj) {
+			if (obj[attr] && typeof obj[attr] == "object") {
+				c[attr] = copy(obj[attr]);
+			} else {
+				c[attr] = obj[attr];
+			}			
+		}
+		
+		return c;
+	}
+
+	
+	
+	/**
+	 * Database API helper - implements error handler for "transaction" function
+	 */	
+	function transaction(callback, successCallback) {
+		var errorCallback = function(e) {
+			settings.LOG_ERROR("Transaction Error: " + e.message);
+		};
+		
+		if (successCallback) {
+			db.transaction(callback, errorCallback, successCallback);
+		} else {
+			db.transaction(callback, errorCallback);
+		}		
+	}
+	
+	
+	
+	/**
+	 * Database API helper - implements error handler for "executeSql" function and optional SQL logging
+	 */	
+	function executeSql(t, sql, params, callback) {
+		if (settings.LOG_SQL) {
+			settings.LOG("SQL: " + sql + " [" + params + "]");
+		}		
+
+		t.executeSql(sql, params, callback, function(t, e) {
+			settings.LOG_ERROR("SQL Error: " + sql + ": [" + params + "]: " + e.message);
+		});
+	}
+
+	
+	
+	/**
+	 * Initializes table synchronization.
+	 */
+	function initTable(table) {
+		var tableName = table.TABLE_NAME;
+		settings.LOG("initTable: " + tableName);
+		
+		transaction(function(t) {
+			
+			executeSql(t, "select * from SQLITE_MASTER where TYPE = 'table' and name = ?",  [tableName], function(t, rs) {
+				
+	    		table.fields = {};
+
+		    	if (rs.rows.length) {
+		            // table exist - parse columns	                
+		    		
+		    		var sql = rs.rows.item(0).sql, fields = sql.match(/\(([^\)]*)\)/)[1].split(",");
+		    		
+		    		for (var i = 0; i < fields.length; i++) {
+		    			var field = fields[i].trim().split(" ");
+		    			
+		    			if (field[0] != IS_DIRTY) {
+		    				table.fields[field[0]] = field[1];
+		    			}
+		    			
+		    			settings.LOG("initTable: " + tableName + "." + field[0] + "=" + field[1]);
+		    		}		    		
+		    		
+		    	} else {
+    	    		settings.LOG("initTable: creating new table " + tableName);
+		    		executeSql(t, "create table " + tableName + " (" + KEY + " text primary key, " + IS_DIRTY + " integer default 1)", []);
+		    		table.fields[key] = TEXT;
+		    	}
+		    			    	
+		    	syncNow(tableName);
+		    }); 
+			
+		});		
+	}
+	
+		
+
+	/** 
+	 * Performs synchronization for table after the configured delay (SYNC_NOW_DELAY_MS setting).
+	 */
+	function syncNow(tableName) {
+		scheduleSync(tableName, settings.SYNC_NOW_DELAY_MS);
+	}
+
+
+	
+	/**
+	 * Schedules synchronization for table after the specified period.
+	 */
+	function scheduleSync(tableName, period) {
+		var ctx = "scheduleSync(" + tableName + "," + period + "): ";
+				
+		var table = settings.TABLES[tableName];
+		if (table.syncTimeout) {
+			clearTimeout(table.syncTimeout);
+		}
+		
+		if (navigator.onLine) {
+			settings.LOG(ctx + "scheduling");
+			table.syncTimeout = setTimeout(function() {
+				delete table.syncTimeout;
+				syncTable(table);
+			}, period);
+		} else {
+			settings.LOG(ctx + "browser is offline, will sync when it's back online");
+			
+			var sync = function() { 
+				settings.LOG(ctx + "browser is now online, syncing");
+				syncTable(table);
+				window.removeEventListener(sync);
+			};
+			
+			window.addEventListener("online", sync);
+		}		
+	}
+	
+	
+	
+	/**
+	 * Synchronizes the given table with AppEngine.
+	 *  
+	 * Sends updates for the given table to Appengine.
+	 * Updates are identified based on is_dirty field.
+	 * 
+	 * Receives updates for the given table from AppEngine.
+	 * Updates are identified based on timestamp field.
+	 */
+	function syncTable(table) {
+		var tableName = table.TABLE_NAME,
+			ctx = "syncTable(" + tableName + "): ",
+			
+			sendBatchSize = table.SEND_BATCH_SIZE,
+			sendTotalCount = 0,			
+			sendSql = "select * from " + tableName + " where " + IS_DIRTY + " > 0", 
+			
+			receiveBatchSize = table.RECEIVE_BATCH_SIZE,
+			receiveTotalCount = 0,
+			receiveState = getState(tableName, RECEIVE),
+			
+			urlBase = settings.ROCKET_URL + "/" + tableName + "?count=" + receiveBatchSize;
+        
+		var syncBatch = function() {
+			var sendCount;
+			var updatesOut = [];
+			
+			transaction(function(t) {
+				executeSql(t, sendSql + " limit " + sendTotalCount + "," + sendBatchSize, [], function(t, rs) {
+					sendCount = rs.rows.length;
+					sendTotalCount += sendCount;
+					
+					settings.LOG(ctx + "Sending " + sendCount + " updates");
+					
+					for (var updatesOutIndex = 0; updatesOutIndex < sendCount; updatesOutIndex++) {
+						var update = rs.rows.item(updatesOutIndex);
+						settings.LOG(ctx + "Sending update, key: " + update[KEY])
+						updatesOut.push(update);
+					}
+				})					
+			}, function() {
+				
+				// successfully retrieved outbound updates from DB				
+
+				// figure out URL for the current batch
+				var url = urlBase;
+			    if (receiveState) {
+			    	url += "&from=" + receiveState;
+			        settings.LOG(ctx + "Receiving records from " + receiveState);
+			    } else {
+			    	settings.LOG(ctx + "Receiving records from beggining");
+			    }			        					
+									
+			    // post outbound updates to server and parse inbound updates
+				$.post(url, {"updates": JSON.stringify(updatesOut)}, function(data) {
+					
+			    	if (data.updates) {
+			    		// all is ok so far, outbound updates have been posted and inbound updates are ready to be processed 
+			    		
+			    		// mark outbound updates as processed
+						transaction(function(t) {
+							for (var updatesOutIndex = 0; updatesOutIndex < sendCount; updatesOutIndex++) {
+								var update = updatesOut[updatesOutIndex];
+								// TODO - probably need some logic to ensure is_dirty doesn't get set below zero
+								executeSql(t, "update " + tableName + " set " + IS_DIRTY + " = " + IS_DIRTY + " - ? where key = ?", [update[IS_DIRTY], update[KEY]]);
+							}
+						}, function() {
+	
+							// process inbound updates
+				        	var updatesIn = data.updates,            		
+				    			receiveCount = updatesIn.length;
+				    	
+				        	receiveTotalCount += receiveCount;
+				        	
+				        	settings.LOG(ctx + "Received " + receiveCount + " updates");
+				        	
+				        	// define function that processes inbound updates 1 by 1
+				        	var receiveUpdate = function(updateInIndex) {
+				        		if (updateInIndex < receiveCount) {
+				            		var update = updatesIn[updateInIndex];
+				            		
+				            		var key = update[KEY];
+				            		
+				            		settings.LOG(ctx + "Received update, key: " + key);
+	
+				        			transaction(function(t) {
+				                		// check if we need to add any field
+				                		for (fieldName in update) {
+				                			var value = update[fieldName];
+				                			
+				                			if (!(fieldName in table.fields) && fieldName != TIMESTAMP) {
+				                				var type;
+				                				if (typeof value == "number") {
+				                					type = "numeric";
+				                				} else  {
+				                					type = "text";
+				                				}
+				                					
+				                				settings.LOG(ctx + "Adding field " + fieldName + " of type " + type);
+				                				executeSql(t, "alter table " + tableName + " add " + fieldName + " " + type);
+				                				table.fields[fieldName] = type;
+				                			}
+				                		}
+				                		
+				        			}, function() {
+	
+				        				// transaction succeeded, insert or update values	
+				        				
+				        				transaction(function(t) {
+				        					executeSql(t, "select " + KEY + ", " + IS_DIRTY + " from " + tableName + " where " + KEY + " = ?", [key], function(t, rs) {
+				        						var sql, params = [];
+				        						if (rs.rows.length > 0) {
+				        							// record already exist
+	
+				        							if (rs.rows.item(0)[IS_DIRTY] > 0) {
+				        								settings.LOG(ctx + "Unsynchronized outbound updates exist, skipping inbound update, key: " + key);
+				        							} else {
+					        							var fields = [];
+					        							for (var fieldName in update) {
+					        								if (fieldName != KEY && fieldName != TIMESTAMP) {
+					        									fields.push(fieldName + "=?");
+					        									params.push(update[fieldName]);
+					        								}
+					        							}
+					        							
+					        							sql = "update " + tableName + " set " + fields.join(",") + " where " + KEY + "=?";
+					        							
+					        							params.push(key);
+				        							}
+				        							
+				        						} else {
+				        							// new record
+				        							
+				        							var fields = [IS_DIRTY], values = [0];
+				        							for (var fieldName in update) {
+				        								if (fieldName != TIMESTAMP) {
+					    									fields.push(fieldName);
+					    									params.push(update[fieldName]);
+					    									values.push("?");
+				        								}
+				        							}
+				        							
+				        							sql = "insert into " + tableName + " (" + fields.join(",") + ") values (" + values.join(",") + ")";             							
+				        						}
+				        						
+				        						executeSql(t, sql, params);
+				        					});
+				        					
+				        				}, function() {
+				        					// transaction succeeded, update state and process next update
+				        					
+				        					receiveState = setState(tableName, RECEIVE, update[TIMESTAMP]);
+				        					
+				        					if (table.ON_RECEIVE) {
+				        						// asynchronously call ON_RECEIVE handler
+				        						setTimeout(function() {
+				        							table.ON_RECEIVE(update);
+				        						}, 0);
+				        					}
+				        					
+				                			receiveUpdate(updateInIndex + 1);
+				        				});
+				            			
+				        			});
+				        			
+				        		} else {
+				        			
+				        			// all inbound updates processed
+				                	
+				                    if (receiveCount < receiveBatchSize) {
+				                		// looks like there are no more inbound updates
+				                    	var batchStartTimestamp = data.timestamp;
+				                		if (!receiveState || batchStartTimestamp > receiveState) {
+				                			receiveState = setState(tableName, RECEIVE, batchStartTimestamp);
+										}			                    		
+				                	}            			
+	
+				                	
+				                    if (sendCount == sendBatchSize || receiveCount == receiveBatchSize) {
+				                    	// sync next batch
+				    	            	syncBatch(); 
+				    	            	
+				                	} else {
+				                		// we are done, schedule next sync cycle
+				            			scheduleSync(tableName, table.SYNC_IDLE_TIME_MS);
+				            		}  
+				        		}
+				        	};
+				        	
+				        	// trigger processing of inbound updates
+				        	receiveUpdate(0);  	
+						});
+
+					} else {
+						settings.LOG(ctx + "Bad response received, browser is probably offline");
+						// schedule next sync cycle
+						scheduleSync(tableName, table.SYNC_IDLE_TIME_MS);
+					}
+				});
+								
+			});
+		};
+		
+		syncBatch();		
+	}
+	
+	
+	
+	/**
+	 * Retrieves synchronization state from localStorage for given table and type (send/receive).
+	 */	
+	function getState(tableName, type) {
+		return localStorage.getItem("rocket__" + tableName + "__" + type);
+	}
+
+
+	
+	/**
+	 * Save synchronization state to localStorage for given table and type (send/receive).
+	 */	
+	function setState(tableName, type, state) {
+		localStorage.setItem("rocket__" + tableName + "__" + type, state);
+		return state;
+	}
+
+	
+
+	/**
+	 * Reset datatabase (drop all tables) and synchronization state.
+	 * Usually should be invoked on logout.
+	 */
+	function reset(settings) {		
+		db = openDatabase(settings.DATABASE_NAME, "", "", 1024*1024);
+		
+		transaction(function(t) {
+			var tables = settings.TABLES;
+			for (var i = 0; i < tables.length; i++) {
+				var table = tables[i];
+				localStorage.removeItem("rocket__" + table + "__" + SEND);
+				localStorage.removeItem("rocket__" + table + "__" + RECEIVE);
+				executeSql(t, "drop table if exists " + table);
+			}
+		});
+	};
+	
+	
+	
+	return {
+		init: init,
+		reset: reset,
+		db: db,
+		syncNow: syncNow,
+		transaction: transaction,
+		executeSql: executeSql,
+	};
+	
+}();
---
+++ rocket.py
@@ -0,0 +1,282 @@
+# Copyright 2010 Kaspars Dancis
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+import logging, base64, os, datetime, time
+
+from google.appengine.api import datastore, datastore_types, datastore_errors
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+import json, yaml
+
+
+
+DATETIME = "datetime"
+TIMESTAMP = "timestamp"
+BOOL = "bool"
+LONG = "long"
+FLOAT = "float"
+INT = "int"
+TEXT = "text"
+KEY = "key"
+LIST = "list"
+STR = "str"
+BLOB = "blob"
+
+KEY = "key"
+TIMESTAMP = "timestamp"
+IS_DIRTY = "is_dirty"
+
+
+
+class Rocket(webapp.RequestHandler):
+    
+    def get_config(self):
+        if not hasattr(self, "config"):
+            rocket_yaml = os.path.join(os.path.dirname(os.path.dirname(__file__)), "rocket.yaml")
+            self.config = yaml.load(file(rocket_yaml, "r"))
+            
+            def import_filter(type):
+                if self.config.has_key(type):                
+                    query_filter_name = self.config[type] 
+                    try:
+                        i = query_filter_name.rfind('.')
+                        if i <= 0:
+                            raise Exception("Config error: %s - no module specified" % type)
+                                                        
+                        p = query_filter_name[:i]
+                        m = query_filter_name[i+1:]
+                        
+                        exec "from %s import %s as filter" % (p, m) in locals()
+                        
+                        return filter
+                    except Exception, e:
+                        raise Exception("Config error: cannot import %s - %s" % (type, e.message))
+                else:
+                    raise Exception("Config error: %s must be specified" % type)    
+            
+            self.query_filter = import_filter("query_filter")
+            self.update_filter = import_filter("update_filter")            
+            
+        return self.config
+    
+    
+    
+    def bad_request(self, error):    
+        self.error(400)
+        logging.error(u"Bad Request: %s" % error)
+        self.response.out.write(json.dumps({"error": error}))
+
+   
+        
+    def post(self):
+        
+        # PARSE CONTEXT AND PARAMETERS
+         
+        self.response.headers['Content-Type'] = 'application/json'
+
+        path = self.request.path.split("/")        
+        if len(path) < 3 or path[2] == '': 
+            return self.bad_request(u'Please specify an entity kind\n')
+                
+        kind = path[2]
+        
+        # PROCESS INBOUND UPDATES
+        
+        keys_in = set()
+        updates_in = json.loads(self.request.get("updates"))
+        
+        entity_config = self.get_config()["entities"][kind]
+        
+        for update in updates_in:
+            key_name_or_id = update[KEY]
+            
+            keys_in.add(key_name_or_id)
+            
+            if key_name_or_id[0] in "0123456789":
+                key = datastore.Key.from_path(kind, int(key_name_or_id)) # KEY ID
+                is_id = True
+            else:                
+                key = datastore.Key.from_path(kind, key_name_or_id) # KEY NAME
+                is_id = False
+            
+            try: 
+                entity = datastore.Get(key)
+
+                if not self.update_filter(self.request, kind, entity):
+                    logging.error("update on existing entity is filter, key: %s" % key_name_or_id)
+                    continue
+                    
+            except datastore_errors.EntityNotFoundError:
+                if is_id:
+                    entity = datastore.Entity(kind=kind,id=int(key_name_or_id))
+                else:
+                    entity = datastore.Entity(kind=kind,name=key_name_or_id)
+                
+            for attr_name in update:
+                if attr_name != KEY and attr_name != TIMESTAMP and attr_name != IS_DIRTY:
+                    if entity_config.has_key(attr_name):
+                        attr_config = entity_config[attr_name]
+                    else:
+                        attr_config = None
+                        
+                    if attr_name == TIMESTAMP:
+                        value = datetime.datetime.now()
+                    else:
+                        value = update[attr_name] 
+                        
+                    entity[attr_name] = js_to_appengine_value(value, attr_config)
+
+            # update timestamp to current time so that it get's picked up by other clients
+            entity[TIMESTAMP] = datetime.datetime.now()                                    
+
+            if not self.update_filter(self.request, kind, entity):
+                logging.error("updated entity is filtered, key: %s" % key_name_or_id)
+                continue
+                    
+            datastore.Put(entity)
+            
+            
+        # PROCESS OUTBOUND UPDATES            
+
+        batch_size = int(self.request.get("count"))
+
+        updates_out = []
+        
+        query = datastore.Query(kind)
+            
+        f = self.request.get("from") 
+        if f: 
+            query['%s >= ' % TIMESTAMP] = datetime_from_iso(f)
+            
+        self.query_filter(self.request, kind, query)
+    
+        query.Order(TIMESTAMP)
+        
+        batch_start_timestamp = appengine_to_js_value(datetime.datetime.now())
+            
+        entities = query.Get(batch_size, 0)
+        
+        for entity in entities:
+            key = appengine_to_js_value(entity.key())
+            
+            # do not send back updates that we just received from the same client
+            if not key in keys_in:                                
+                update = {
+                    KEY: key
+                }
+                
+                for field, value in entity.items():                        
+                    update[field] = appengine_to_js_value(value)
+                            
+                updates_out.append(update)
+                        
+        res = json.dumps({
+            "updates": updates_out, 
+            "timestamp": batch_start_timestamp,
+        })
+        
+        self.response.out.write(res)
+
+
+
+
+def appengine_to_js_value(value):
+    # DATETIME
+    if isinstance(value, datetime.datetime):
+        return datetime_to_iso(value)
+    
+    # BOOL
+    elif isinstance(value, bool):
+        return int("%d" % value)
+    
+    # KEY
+    elif isinstance(value, datastore_types.Key):
+        return value.id_or_name()
+    
+    # LIST
+    if isinstance(value, list):
+        return map(lambda value: appengine_to_js_value(value), list)
+    
+    # KEY
+    elif isinstance(value, datastore_types.Blob):
+        return base64.b64encode(value)
+    
+    # ALL OTHERS
+    else:
+        return value
+
+
+
+def js_to_appengine_value(value, attr_config):
+    if not value or not attr_config:
+        return None
+    
+    type = attr_config['type']
+    if type == DATETIME:
+        return datetime_from_iso(value)
+        
+    elif type == INT:
+        return int(value)
+    
+    elif type == LONG:
+        return long(value)
+    
+    elif type == BOOL:
+        return bool(value)
+        
+    elif type == TEXT:
+        return datastore_types.Text(value)
+        
+    elif type == KEY:
+        kind = attr_config['kind']
+        if value[0] in "0123456789":
+            return datastore.Key.from_path(kind, int(value))
+        else:            
+            return datastore.Key.from_path(kind, value)
+          
+    elif type == BLOB:
+        return datastore_types.Blob(base64.b64decode(value))
+
+    elif type == LIST:
+        return map(lambda value: js_to_appengine_value(value, attr_config['items']), value.split("|"))
+        
+    else: #str
+        return value
+    
+    
+    
+def datetime_from_iso(s):
+    dt = datetime.datetime.strptime(s[:19], "%Y-%m-%dT%H:%M:%S")
+    try: dt = dt.replace(microsecond = int(s[20:]))
+    except: pass
+    return dt
+
+
+
+def datetime_to_iso(dt):
+    return dt.isoformat()    
+                                                                
+             
+
+application = webapp.WSGIApplication([('/rocket/.*', Rocket)], debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
